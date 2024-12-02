261930c4e9336530dcb682eee5b8a0a5b6cd31be
Eva Vast <eva.vast@outlook.com>
2024-12-02 20:15:10 +0100

    Finished day 1 part 1, added information about day 2

--- 2024/day1/day1.py
+++ 2024/day1/day1.py
@@ -1,5 +1,24 @@
 def main():
+    f = open("2024/day1/input_eva.txt")
+    print("Using file " + f.name)
+    lines = f.readlines()
     
+    left_nums = []
+    right_nums = []
+
+    for line in lines:
+        numbers = line.split()
+        left_nums.append(numbers[0])
+        right_nums.append(numbers[1])
+    
+    left_nums.sort()
+    right_nums.sort()
+
+    diff = 0
+    for i in range(0, len(left_nums)):
+        diff += abs(int(left_nums[i]) - int(right_nums[i]))
+
+    print(diff)
     return 0
 
 
