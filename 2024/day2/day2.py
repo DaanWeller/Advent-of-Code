9f211191f83572f5cb2448e9b3510c9ad787fa8a
Eva Vast <eva.vast@outlook.com>
2024-12-16 21:59:22 +0100

    Added solution for part 2 and instructions for part 3

--- 2024/day2/day2.py
+++ 2024/day2/day2.py
@@ -3,47 +3,94 @@ def main():
     print("Using file " + f.name)
     reports = f.readlines()
 
-    sum = 0
+    sum_pt1 = 0
+    sum_pt2 = 0
     for report in reports:
-        if is_safe(report):
-            sum += 1
+        if is_report_safe(report):
+            sum_pt1 += 1
+        
+        if is_report_safe_dampened(report):
+            sum_pt2 += 1
     
-    print("The answer to part 1 is " + str(sum))   
+    print(reports[-1])
+
+    print("The answer to part 1 is " + str(sum_pt1))   
+
+    print("The answer to part 2 is " + str(sum_pt2))   
 
     return 0
 
 
-def is_safe(report: str):
+def is_report_safe_dampened(report: str):    
+    lvls = report.split()
+
+    lvls_reversed = lvls.copy()
+    lvls_reversed.reverse()
+
+    return sliding_window(lvls, True)
+
+
+def sliding_window(lvls: list, joker: bool):
+    increasing = (int(lvls[1]) > int(lvls[0]))
+
+    for i in range(len(lvls) - 1):
+        prev_lvl = int(lvls[i])
+
+        lvl = int(lvls[i+1])
+
+        if not is_lvl_safe(increasing, prev_lvl, lvl):
+            if joker:
+                lvls_0 = lvls.copy()
+                lvls_0.pop(0)
+                lvls_1 = lvls.copy()
+                lvls_1.pop(i)
+                lvls_2 = lvls.copy()
+                lvls_2.pop(i+1)
+
+                if sliding_window(lvls_1, False) or sliding_window(lvls_2, False) or sliding_window(lvls_0, False):
+                    return True
+
+
+                # return (sliding_window(lvls_1, False) or sliding_window(lvls_2, False))
+            
+            return False
+
+    return True
+
+ 
+def is_lvl_safe(increasing: bool, prev_lvl: int, lvl: int):
+
+    safe = ( (lvl > prev_lvl) == increasing)
+    
+    if abs(lvl - prev_lvl) < 1 or abs(lvl - prev_lvl) > 3:
+        safe = False
+
+    return safe
+
+
+
+def is_report_safe(report: str):
     safe = True
     
     lvls = report.split()
 
     prev_lvl = int(lvls[0])
 
-    if(int(lvls[1]) > prev_lvl):
-        increasing = True
-    else:
-        increasing = False
-        
+    increasing = (int(lvls[1]) > int(lvls[0]))        
        
     for i in range(len(lvls) -1):
 
         lvl = int(lvls[i+1])
 
-        if lvl > prev_lvl and not increasing:
+        if not is_lvl_safe(increasing, prev_lvl, lvl):
             safe = False
-            break
-        elif lvl < prev_lvl and increasing:
-            safe = False
-            break
-
-        if abs(lvl - prev_lvl) < 1 or abs(lvl - prev_lvl) > 3:
-            safe = False
-            break
+            return safe
 
         prev_lvl = lvl
     
     return safe
+
+
 
 
 if __name__ == "__main__":
---
+++ 2024/day3/day3.py
@@ -0,0 +1,8 @@
+def main():
+    f = open("2024/day2/input_eva.txt")
+    print("Using file " + f.name)
+    
+    return 0
+
+if __name__ == "__main__":
+    main()
